6802790f55e85fa2e6a65bcd4deafa6d3ba15e25
Helen Virtanen <helenvirtanen28@gmail.com>
2025-06-07 15:44:43 +0300

    added dropdown menu in profile

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,75 +1,132 @@
 "use client";
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import { Bookmark, LayoutDashboard, Heart, LogIn, UserPlus } from 'lucide-react';
-import { usePathname } from 'next/navigation';
-import { useUser } from '@auth0/nextjs-auth0';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  Bookmark,
+  LayoutDashboard,
+  Heart,
+  LogIn,
+  UserPlus,
+} from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useUser } from "@auth0/nextjs-auth0";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 function Header() {
-    const { user, isLoading } = useUser();
-    console.log('User: ', user)
+  const { user, isLoading } = useUser();
+  console.log("User: ", user);
 
-    const pathname = usePathname();
-    
-    const menu = [
-        {
-            name: "Browse",
-            link: "/",
-            icon: <LayoutDashboard size={20} />
-        },
-        {
-            name: "Favourites",
-            link: "/favourites",
-            icon: <Heart size={22} />
-        },
-        {
-            name: "Saved",
-            link: "/bookmarks",
-            icon: <Bookmark size={20} />
-        }
-    ];
+  const pathname = usePathname();
+
+  const menu = [
+    {
+      name: "Browse",
+      link: "/",
+      icon: <LayoutDashboard size={20} />,
+    },
+    {
+      name: "Favourites",
+      link: "/favourites",
+      icon: <Heart size={22} />,
+    },
+    {
+      name: "Saved",
+      link: "/bookmarks",
+      icon: <Bookmark size={20} />,
+    },
+  ];
+
+  if (isLoading) return null;
 
   return (
     <header className="min-h-[10vh] px-16 py-6 w-full bg-white flex justify-between items-center shadow-sm">
-        <Link href="/">
-          <Image src={'/pokemon--logo.png'} alt='Pokemon logo' width={120} height={90} />
-        </Link >
-        <nav>
-            <ul className='flex items-center gap-8 text-gray-400'>
-                {menu.map((item, index:number) => (
-                    <li key={index}>
-                        <Link 
-                            href={item.link}
-                            className={`py-2 px-6 text-sm flex items-center gap-2 font-bold rounded-lg
+      <Link href="/">
+        <Image
+          src={"/pokemon--logo.png"}
+          alt="Pokemon logo"
+          width={120}
+          height={90}
+        />
+      </Link>
+      <nav>
+        <ul className="flex items-center gap-8 text-gray-400">
+          {menu.map((item, index: number) => (
+            <li key={index}>
+              <Link
+                href={item.link}
+                className={`py-2 px-6 text-sm flex items-center gap-2 font-bold rounded-lg
                                 ${
-                                    pathname === item.link
-                                    ? 'bg-[#6c5ce7]/15 text-[#6c5ce7]'
-                                    : ''
+                                  pathname === item.link
+                                    ? "bg-[#6c5ce7]/15 text-[#6c5ce7]"
+                                    : ""
                                 }
                             `}
-                            >
-                            <span>{item.icon}</span>
-                            <span>{item.name}</span>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+              >
+                <span>{item.icon}</span>
+                <span>{item.name}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
 
-        {user?.sub && !isLoading && <div></div>}
-        {!user?.sub && !isLoading && 
-            (<div className='flex items-center gap-4'>
-            <Link href='/auth/login' className='py-2 px-3 text-sm flex items-center gap-2 font-bold rounded-lg bg-[#6c5ce7]/15 text-[#6c5ce7] hover:bg-[#6c5ce7]/30 transition-all duration-300 ease-in-out'>
-                <LogIn size={20} />Login
-            </Link>
-            <Link href='/auth/login' className='py-2 px-3 text-sm flex items-center gap-2 font-bold rounded-lg bg-[#6c5ce7] text-white hover:bg-[#6c5ce7]/90 transition-all duration-300 ease-in-out'>
-                <UserPlus size={20} />Sign up
-            </Link>
-            </div>)
-        }
+      {user?.sub && !isLoading && (
+        <div>
+          <DropdownMenu modal={false}>
+            <DropdownMenuTrigger className="outline-none border-none">
+              <div className="bg-[#6c5ce7]/15 flex items-center justify-center gap-2 rounded-lg cursor-pointer">
+                <span className="pl-2 text-[#6c5ce7] text-sm font-bold">
+                  {user?.name || "User"}
+                </span>
+                <Image
+                  src={user?.picture || "/avatar.png"}
+                  width={40}
+                  height={40}
+                  alt="avater"
+                  className="p-1 rounded-lg cursor-pointer"
+                />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      )}
+      {!user?.sub && !isLoading && (
+        <div className="flex items-center gap-4">
+          <Link
+            href="/auth/login"
+            className="py-2 px-3 text-sm flex items-center gap-2 font-bold rounded-lg bg-[#6c5ce7]/15 text-[#6c5ce7] hover:bg-[#6c5ce7]/30 transition-all duration-300 ease-in-out"
+          >
+            <LogIn size={20} />
+            Login
+          </Link>
+          <Link
+            href="/auth/login"
+            className="py-2 px-3 text-sm flex items-center gap-2 font-bold rounded-lg bg-[#6c5ce7] text-white hover:bg-[#6c5ce7]/90 transition-all duration-300 ease-in-out"
+          >
+            <UserPlus size={20} />
+            Sign up
+          </Link>
+        </div>
+      )}
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
